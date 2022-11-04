27e19fdb176e67ccf295aa05821b4f36b0897ef3
CORP\luizfrib <luiz-fernando.ribeiro@capgemini.com>
2022-11-04 09:56:52 +0000

    Creating animation method.

--- Frog/code/player.py
+++ Frog/code/player.py
@@ -3,14 +3,20 @@ import pygame
 class Player(pygame.sprite.Sprite):
     def __init__(self, pos, groups):
         super().__init__(groups)
-        self.image = pygame.Surface((50,50))
-        self.image.fill('red')
+
+        self.import_assets()
+        self.frame_index = 0
+        self.image = self.animation[self.frame_index]
         self.rect = self.image.get_rect(center = pos)
 
         # float based movement
         self.pos = pygame.math.Vector2(self.rect.center)
         self.direction = pygame.math.Vector2()
         self.speed = 200
+
+    def import_assets(self):
+        path = '../graphics/player/right/'
+        self.animation = [pygame.image.load(f'{path}{frame}.png').convert_alpha() for frame in range(4)]  
 
     def move(self, dt):
         # normalize a vector
@@ -37,6 +43,13 @@ class Player(pygame.sprite.Sprite):
         else:
             self.direction.y = 0            
 
+    def animate(self,dt):
+        self.frame_index += 10 * dt
+        if self.frame_index >= len(self.animation):
+            self.frame_index = 0
+        self.image = self.animation[int(self.frame_index)]
+
     def update(self, dt):
         self.input()
         self.move(dt)
+        self.animate(dt)
